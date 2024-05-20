f8307333c63e89b9494f06ebabadf1c5fd874d79
alxgitwe <el.najib15@gmail.com>
2024-05-19 19:13:06 -0700

    m

---
+++ models/base_model.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""model"""
+import models
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
